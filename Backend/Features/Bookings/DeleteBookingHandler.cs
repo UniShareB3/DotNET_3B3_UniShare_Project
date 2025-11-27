3f5ca2467b0ec833ef3b1020a89194ab985ec482
biancamilea04 <161333127+biancamilea04@users.noreply.github.com>
2025-11-27 11:26:44 +0200

    add DeleteBookingEndpoint

---
+++ Backend/Features/Bookings/DeleteBookingHandler.cs
@@ -0,0 +1,24 @@
+﻿using Backend.Features.Booking.DTO;
+using Backend.Persistence;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Features.Booking;
+
+public class DeleteBookingHandler(ApplicationContext context): IRequestHandler<DeleteBookingRequest,IResult>
+{
+    public async Task<IResult> Handle(DeleteBookingRequest request, CancellationToken cancellationToken)
+    {
+        var booking = await context.Bookings.FirstOrDefaultAsync(i => i.Id == request.Id, cancellationToken);
+        if (booking == null)
+        {
+            throw new KeyNotFoundException("Booking not found");
+        }
+
+        context.Bookings.Remove(booking);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return Results.NoContent();
+    }
+}
---
+++ Backend/Features/Bookings/GetBookingHandler.cs
@@ -0,0 +1,17 @@
+﻿using Backend.Persistence;
+using MediatR;
+
+namespace Backend.Features.Booking;
+
+public class GetBookingHandler(ApplicationContext dbContext) : IRequestHandler<GetBookingRequest, IResult>
+{
+    public Task<IResult> Handle(GetBookingRequest request, CancellationToken cancellationToken)
+    {
+        var booking = dbContext.Bookings.Find(request.BookingId);
+        if (booking == null)
+        {
+            return Task.FromResult(Results.NotFound());
+        }
+        return Task.FromResult(Results.Ok(booking));
+    }
+}
