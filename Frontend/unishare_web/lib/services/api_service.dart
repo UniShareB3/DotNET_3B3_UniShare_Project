2a23e6d6df628c134fd6a2ce1b4318c4abfce85e
biancamilea04 <161333127+biancamilea04@users.noreply.github.com>
2025-12-14 13:38:52 +0200

    test if the base_url is load in production

--- Frontend/unishare_web/lib/services/api_service.dart
+++ Frontend/unishare_web/lib/services/api_service.dart
@@ -7,7 +7,10 @@ import 'package:unishare_web/services/secure_storage_service.dart';
 import '../main.dart';
 
 class ApiService {
-  static String get baseUrl => dotenv.env['API_BASE_URL'] ?? 'http://localhost:5083';
+  final baseUrl = dotenv.env['API_BASE_URL'];
+  if (baseUrl == null || baseUrl.isEmpty) {
+    throw Exception('CRITIC: Nu am gasit API_BASE_URL in fisierul .env!');
+  }
 
   // Debug: store last non-200 response body for received bookings (serialization/server issues)
   static String? lastReceivedError;
